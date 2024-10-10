ee39b78dab31ee4191fb38ee70a9ac9f32a20431
Xcutor <Lekanabass50@gmail.com>
2024-10-09 22:46:09 -0500

    task 4 done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,9 +15,35 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+interface studentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
 const printTeacher:printTeacherFunction = (firstName: string, lastName: string): string => {
   let result: string;
 
   result = firstName.charAt(0) + '. ' + lastName;
   return (result);
 }
+
+class StudentClass implements studentClassInterface{
+  private firstName!: string;
+  private lastName!: string;
+ 
+  constructor(firstName: string, lastName: string) {
+  	this.firstName = firstName;
+	this.lastName = lastName;
+  }
+  
+  workOnHomework() {
+    return 'Currently working';
+  }
+
+  displayName() {
+    return this.firstName;
+  }
+}
+
+let std = new StudentClass('Abdulahi', 'Abass');
+console.log(std.displayName());
